71b5ddc87368f076977cf4924f652031a17d21be
G3nD4 <66173686+G3nD4@users.noreply.github.com>
2023-12-22 13:50:15 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,6 @@ void main() async {
   // Run the app and pass in the SettingsController. The app listens to the
   // SettingsController for changes, then passes it further down to the
   // SettingsView.
-
-
-
   
   runApp(MyApp(settingsController: settingsController));
 }
